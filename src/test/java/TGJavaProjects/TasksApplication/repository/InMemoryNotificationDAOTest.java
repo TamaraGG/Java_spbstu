54bfc79d0ac886739fdfb8a3da48a855c58d2512
Tamara Goncharova <goncharova2804t@gmail.com>
2025-03-18 17:01:29 +0300

    InMemoryNotificationDAOTest added, InMemoryNotificationDAOTest beforeEach method altered

--- src/test/java/TGJavaProjects/TasksApplication/repository/InMemoryNotificationDAOTest.java
+++ src/test/java/TGJavaProjects/TasksApplication/repository/InMemoryNotificationDAOTest.java
@@ -1,4 +1,80 @@
+package TGJavaProjects.TasksApplication.repository;
+
+import TGJavaProjects.TasksApplication.model.Notification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class InMemoryNotificationDAOTest {
-  
+
+    private static final Notification notification1 = mock(Notification.class);
+    private static final Notification notification2 = mock(Notification.class);
+
+    private static final long USER_ID = 4L;
+    private static final long TASK_ID = 2L;
+
+    private InMemoryNotificationDAO repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new InMemoryNotificationDAO();
+    }
+
+    @Test
+    void InMemoryNotificationDAO_GetAllNotifications_ReturnListOfNotifications() {
+        repository.addNotification(notification1);
+        repository.addNotification(notification2);
+
+        List<Notification> recievedNotifications = repository.getAllNotifications();
+
+        assertNotNull(recievedNotifications);
+        assertEquals(2, recievedNotifications.size());
+    }
+
+    @Test
+    void InMemoryNotificationDAO_GetUserNotifications_ReturnListOfNotifications() {
+        when(notification1.getUserId()).thenReturn(USER_ID);
+        when(notification2.getUserId()).thenReturn(USER_ID);
+        repository.addNotification(notification1);
+        repository.addNotification(notification2);
+
+        List<Notification> recievedNotifications = repository.getUserNotifications(USER_ID);
+
+        assertNotNull(recievedNotifications);
+        assertEquals(2, recievedNotifications.size());
+    }
+
+    @Test
+    void InMemoryNotificationDAO_GetTaskNotifications_ReturnListOfNotifications() {
+        when(notification1.getTaskId()).thenReturn(TASK_ID);
+        when(notification2.getTaskId()).thenReturn(TASK_ID);
+        repository.addNotification(notification1);
+        repository.addNotification(notification2);
+
+        List<Notification> recievedNotifications = repository.getTaskNotifications(TASK_ID);
+
+        assertNotNull(recievedNotifications);
+        assertEquals(2, recievedNotifications.size());
+    }
+
+    @Test
+    void InMemoryNotificationDAO_AddNotification_ReturnAddedTask() {
+
+        Notification addedNotification = repository.addNotification(notification1);
+
+        assertNotNull(addedNotification);
+        assertEquals(notification1, addedNotification);
+
+    }
 }
