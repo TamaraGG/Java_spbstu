6dc966bfe6269aaf847556f60ee255b347ad0975
Tamara Goncharova <goncharova2804t@gmail.com>
2025-04-01 17:52:58 +0300

    changed returning value for getTasksByUserId method to ResponseEntity

--- src/main/java/TGJavaProjects/TasksApplication/controller/TaskController.java
+++ src/main/java/TGJavaProjects/TasksApplication/controller/TaskController.java
@@ -16,35 +16,37 @@ import java.util.List;
 @AllArgsConstructor
 public class TaskController {
 
-    private final TaskService TASKS_SERVICE;
+    private final TaskService taskService;
 
     @GetMapping
     public List<Task> getAllTasks () {
-        return TASKS_SERVICE.getAllTasks();
+        return taskService.getAllTasks();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable("id") long taskId) {
-        return TASKS_SERVICE.getTaskById(taskId)
+        return taskService.getTaskById(taskId)
                 .map(t-> ResponseEntity.status(HttpStatus.OK).body(t))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/user/{Id}")
-    public List<Task> getTasksByUserId(@PathVariable("id") long userId) {
-        return TASKS_SERVICE.getTasksByUserId(userId);
+    public ResponseEntity<List<Task>> getTasksByUserId(@PathVariable("id") long userId) {
+        return taskService.getTasksByUserId(userId)
+                .map(tasks -> new ResponseEntity<>(tasks, HttpStatus.OK))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<Task> addTask (@RequestBody Task task) {
-        return TASKS_SERVICE.addTask(task)
+        return taskService.addTask(task)
                 .map(t -> ResponseEntity.created(URI.create("/tasks/" + t.getTaskId())).body(t))
                 .orElse(ResponseEntity.badRequest().build());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Task> deleteTask(@PathVariable("id") long taskId) {
-        return TASKS_SERVICE.deleteTask(taskId)
+        return taskService.deleteTask(taskId)
                 .map(t-> new ResponseEntity<>(t, HttpStatus.OK))
                 .orElse(ResponseEntity.badRequest().build());
     }
