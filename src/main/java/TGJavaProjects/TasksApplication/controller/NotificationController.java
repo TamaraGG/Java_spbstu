b965b53fb6efe1bb440a76de8243fec267f5a606
Tamara Goncharova <goncharova2804t@gmail.com>
2025-04-01 17:40:16 +0300

    changed returning value for getUserNotifications and getTaskNotifications to Optional

--- src/main/java/TGJavaProjects/TasksApplication/controller/NotificationController.java
+++ src/main/java/TGJavaProjects/TasksApplication/controller/NotificationController.java
@@ -1,12 +1,16 @@
 package TGJavaProjects.TasksApplication.controller;
 
 import TGJavaProjects.TasksApplication.model.Notification;
+import TGJavaProjects.TasksApplication.model.User;
 import TGJavaProjects.TasksApplication.service.NotificationService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -16,23 +20,8 @@ public class NotificationController {
     private final NotificationService NOTIFICATION_SERVICE;
 
     @GetMapping
-    public List<Notification> getAllNotifications() {
-        return NOTIFICATION_SERVICE.getAllNotifications();
-    }
-
-
-    @GetMapping("/{userId}")
-    public List<Notification> getUserNotifications(@RequestBody long userId) {
-        return NOTIFICATION_SERVICE.getUserNotifications(userId);
-    }
-
-    @GetMapping("/{taskId}")
-    public List<Notification> getTaskNotifications(@RequestBody long taskId) {
-        return NOTIFICATION_SERVICE.getTaskNotifications(taskId);
-
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<Notification>> getUserNotifications(@PathVariable long userId) {
-        List<Notification> result = NOTIFICATION_SERVICE.getUserNotifications(userId);
+    public ResponseEntity<List<Notification>> getAllNotifications() {
+        List<Notification> result = NOTIFICATION_SERVICE.getAllNotifications();
 
         if (result.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -40,18 +29,23 @@ public class NotificationController {
         else {
             return new ResponseEntity<>(result, HttpStatus.OK);
         }
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Notification>> getUserNotifications(@PathVariable long userId) {
+        Optional<List<Notification>> result = NOTIFICATION_SERVICE.getUserNotifications(userId);
+
+        return result
+                .map(notifications -> new ResponseEntity<>(notifications, HttpStatus.OK))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("task/{taskId}")
     public ResponseEntity<List<Notification>> getTaskNotifications(@PathVariable long taskId) {
-        List<Notification> result = NOTIFICATION_SERVICE.getTaskNotifications(taskId);
+        Optional<List<Notification>> result = NOTIFICATION_SERVICE.getTaskNotifications(taskId);
 
-        if (result.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        else {
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        }
-
+        return result
+                .map(notifications -> new ResponseEntity<>(notifications, HttpStatus.OK))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
