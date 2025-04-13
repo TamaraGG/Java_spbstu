83c6e4388779c4869af76322063704ec4e6f647d
TamaraGG <goncharova2804t@gmail.com>
2025-04-13 12:35:07 +0300

    changed InMemoryNotificationImplTest class

---
+++ src/test/java/TGJavaProjects/TasksApplication/service/Implementations/InMemoryTaskServiceImplTest.java
@@ -0,0 +1,33 @@
+package TGJavaProjects.TasksApplication.service.Implementations;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryTaskServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllTasks() {
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void getTasksByUserId() {
+    }
+
+    @Test
+    void addTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+}
