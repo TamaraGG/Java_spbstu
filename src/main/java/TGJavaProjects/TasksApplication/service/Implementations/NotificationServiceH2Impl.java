08c59822c7ab124b98727d2ce10186b59cb67b67
TamaraGG <goncharova2804t@gmail.com>
2025-04-14 02:26:40 +0300

    created class for h2 service

--- src/main/java/TGJavaProjects/TasksApplication/service/Implementations/NotificationServiceH2Impl.java
+++ src/main/java/TGJavaProjects/TasksApplication/service/Implementations/NotificationServiceH2Impl.java
@@ -1,4 +1,30 @@
 package TGJavaProjects.TasksApplication.service.Implementations;
 
-public class NotificationServiceH2Impl {
+import TGJavaProjects.TasksApplication.model.Notification;
+import TGJavaProjects.TasksApplication.service.NotificationService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@Profile("H2")
+public class NotificationServiceH2Impl implements NotificationService {
+    @Override
+    public List<Notification> getAllNotifications() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<List<Notification>> getUserNotifications(long userId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<Notification>> getTaskNotifications(long taskId) {
+        return Optional.empty();
+    }
 }
