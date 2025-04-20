8538f6d29f5f88c30779fc7f6df55023601d5be9
TamaraGG <goncharova2804t@gmail.com>
2025-04-20 16:38:15 +0300

    completed UserServiceH2Impl class

--- src/main/java/TGJavaProjects/TasksApplication/service/Implementations/UserServiceH2Impl.java
+++ src/main/java/TGJavaProjects/TasksApplication/service/Implementations/UserServiceH2Impl.java
@@ -1,6 +1,8 @@
 package TGJavaProjects.TasksApplication.service.Implementations;
 
+import TGJavaProjects.TasksApplication.model.Task;
 import TGJavaProjects.TasksApplication.model.User;
+import TGJavaProjects.TasksApplication.repository.UserRepository;
 import TGJavaProjects.TasksApplication.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Profile;
@@ -13,28 +15,39 @@ import java.util.Optional;
 @AllArgsConstructor
 @Profile("H2")
 public class UserServiceH2Impl implements UserService {
+
+    private final UserRepository userRepository;
+
     @Override
     public List<User> findAllUsers() {
-        return List.of();
+        return userRepository.findAll();
     }
 
     @Override
     public Optional<User> addUser(User user) {
+        if (userRepository.findById(user.getUserId()).isEmpty()) {
+            return Optional.of(userRepository.save(user));
+        }
         return Optional.empty();
     }
 
     @Override
     public Optional<User> findUserById(long userId) {
-        return Optional.empty();
+        return userRepository.findById(userId);
     }
 
     @Override
     public Optional<User> updateUser(User user) {
-        return Optional.empty();
+        if (userRepository.findById(user.getUserId()).isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(userRepository.save(user));
     }
 
     @Override
     public Optional<User> deleteUser(long userId) {
-        return Optional.empty();
+        Optional<User> deletedUser = userRepository.findById(userId);
+        userRepository.deleteById(userId);
+        return deletedUser;
     }
 }
