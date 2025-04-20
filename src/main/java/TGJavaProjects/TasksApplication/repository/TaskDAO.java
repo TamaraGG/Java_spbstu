c9464d0d2609b2526d40c4bcad2920fbf7a3ca61
TamaraGG <goncharova2804t@gmail.com>
2025-04-20 16:13:29 +0300

    renamed h2 repositories

--- src/main/java/TGJavaProjects/TasksApplication/repository/TaskDAO.java
+++
@@ -1,9 +0,0 @@
-package TGJavaProjects.TasksApplication.repository;
-
-import TGJavaProjects.TasksApplication.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface H2TaskDAO extends JpaRepository<Task, Long> {
-}
