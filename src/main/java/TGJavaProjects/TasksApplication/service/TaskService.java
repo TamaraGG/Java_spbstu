938ac0494429bc3a5a9f3d8fac0b3ed9b75380a0
TamaraGG <goncharova2804t@gmail.com>
2025-04-22 13:16:49 +0300

    updated task service

--- src/main/java/TGJavaProjects/TasksApplication/service/TaskService.java
+++ src/main/java/TGJavaProjects/TasksApplication/service/TaskService.java
@@ -4,14 +4,13 @@ import TGJavaProjects.TasksApplication.model.Task;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public interface TaskService {
 
     public List<Task> getAllTasks();
-    public Optional<Task> getTaskById(long taskId);
-    public Optional<List<Task>> getTasksByUserId(long userId);
-    public Optional<Task> addTask (Task task);
-    public Optional<Task> deleteTask(long taskId);
+    public Task findTaskById(long taskId);
+    public List<Task> findTasksByUserId(long userId);
+    public Task addTask (Task task);
+    public void deleteTask(long taskId);
 }
