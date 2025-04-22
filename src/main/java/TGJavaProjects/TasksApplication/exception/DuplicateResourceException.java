65b7e37849a724603c7000da483564321ea2e4c8
TamaraGG <goncharova2804t@gmail.com>
2025-04-22 12:15:27 +0300

    added exceptions

--- src/main/java/TGJavaProjects/TasksApplication/exception/DuplicateResourceException.java
+++ src/main/java/TGJavaProjects/TasksApplication/exception/DuplicateResourceException.java
@@ -3,9 +3,11 @@ package TGJavaProjects.TasksApplication.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.CONFLICT)
-public class DuplicateResourceException extends RuntimeException{
+// Exception for when a resource already exists (e.g., duplicate ID)
+@ResponseStatus(HttpStatus.CONFLICT) // 409 Conflict
+public class DuplicateResourceException extends RuntimeException {
     public DuplicateResourceException(String message) {
         super(message);
     }
 }
+
