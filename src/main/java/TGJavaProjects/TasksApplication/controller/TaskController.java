a37dde877926c007831bef2f94bec31edcf81672
TamaraGG <goncharova2804t@gmail.com>
2025-04-22 13:57:52 +0300

    updated task controller

--- src/main/java/TGJavaProjects/TasksApplication/controller/TaskController.java
+++ src/main/java/TGJavaProjects/TasksApplication/controller/TaskController.java
@@ -1,12 +1,14 @@
 package TGJavaProjects.TasksApplication.controller;
 
+import TGJavaProjects.TasksApplication.exception.DuplicateResourceException;
+import TGJavaProjects.TasksApplication.exception.ResourceNotFoundException;
 import TGJavaProjects.TasksApplication.model.Task;
 import TGJavaProjects.TasksApplication.service.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 import java.util.List;
@@ -24,31 +26,54 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable("id") long taskId) {
-        return taskService.getTaskById(taskId)
-                .map(t-> ResponseEntity.status(HttpStatus.OK).body(t))
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Task> findTaskById(@PathVariable("id") long taskId)
+        throws ResourceNotFoundException {
+
+        try {
+            Task task = taskService.findTaskById(taskId);
+            return ResponseEntity.ok(task);
+        } catch (ResourceNotFoundException e) {
+            throw e;
+        }
     }
 
     @GetMapping("/user/{userId}")
-    public ResponseEntity<List<Task>> getTasksByUserId(@PathVariable long userId) {
-        return taskService.getTasksByUserId(userId)
-                .map(tasks -> new ResponseEntity<>(tasks, HttpStatus.OK))
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<List<Task>> findTasksByUserId(@PathVariable long userId)
+        throws ResourceNotFoundException {
+        try {
+            List<Task> tasks = taskService.findTasksByUserId(userId);
+            return ResponseEntity.ok(tasks);
+        } catch (ResourceNotFoundException e) {
+            throw e;
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Task> addTask (@RequestBody Task task) {
-        return taskService.addTask(task)
-                .map(t -> ResponseEntity.created(URI.create("/tasks/" + t.getTaskId())).body(t))
-                .orElse(ResponseEntity.badRequest().build());
+    public ResponseEntity<Task> addTask (@RequestBody Task task)
+        throws ResponseStatusException, DuplicateResourceException {
+        try {
+            Task createdTask = taskService.addTask(task);
+            return ResponseEntity
+                    .created(URI.create("/api/v1/tasks/" + createdTask.getTaskId()))
+                    .body(createdTask);
+        } catch (ResourceNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e); // 400 Bad Request might be more appropriate than 404 here
+        } catch (DuplicateResourceException e) {
+            throw e;
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e); // 400 Bad Request
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Task> deleteTask(@PathVariable("id") long taskId) {
-        return taskService.deleteTask(taskId)
-                .map(t-> new ResponseEntity<>(t, HttpStatus.OK))
-                .orElse(ResponseEntity.badRequest().build());
+    public ResponseEntity<Task> deleteTask(@PathVariable("id") long taskId)
+        throws ResourceNotFoundException {
+        try {
+            taskService.deleteTask(taskId);
+            return ResponseEntity.noContent().build();
+        } catch (ResourceNotFoundException e) {
+            throw e;
+        }
     }
 
 
