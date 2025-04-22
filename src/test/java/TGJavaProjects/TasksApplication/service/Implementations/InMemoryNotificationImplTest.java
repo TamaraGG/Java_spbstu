177d74c26effe575e7c7fdd0f65832fa1487e848
TamaraGG <goncharova2804t@gmail.com>
2025-04-22 17:34:34 +0300

    changed tests

--- src/test/java/TGJavaProjects/TasksApplication/service/Implementations/InMemoryNotificationImplTest.java
+++ src/test/java/TGJavaProjects/TasksApplication/service/Implementations/InMemoryNotificationImplTest.java
@@ -45,97 +45,97 @@ class InMemoryNotificationImplTest {
     private User user;
     private Task task;
 
-    @BeforeEach
-    void setUp() {
-        notification = new Notification(
-                "test notification", 1L,
-                LocalDateTime.now(), 1L);
-        user = new User(
-                1L,
-                "Jane",
-                "Doe",
-                "test@gmail.com"
-        );
-        task = new Task(
-                1L, "test task",
-                LocalDateTime.now(), LocalDateTime.now(), true, 1L
-        );
-    }
-
-    @Test
-    void getAllNotifications_ReturnsListOfNotifications_WhenNotEmpty() {
-
-        when(notificationDAO.getAllNotifications())
-                .thenReturn(List.of(notification));
-
-        List<Notification> resNotifications = notificationService.getAllNotifications();
-
-        assertEquals(1, resNotifications.size());
-        verify(notificationDAO, times(1)).getAllNotifications();
-    }
-
-    @Test
-    void getAllNotifications_ReturnsListOfNotifications_WhenEmpty() {
-
-        when(notificationDAO.getAllNotifications())
-                .thenReturn(List.of());
-
-        List<Notification> resNotifications = notificationService.getAllNotifications();
-
-        assertEquals(0, resNotifications.size());
-        verify(notificationDAO, times(1)).getAllNotifications();
-    }
-
-    @Test
-    void getUserNotifications_ReturnsListOfUserNotifications_WhenUserExists() {
-        when(userService.findUserById(notification.getUserId()))
-                .thenReturn(Optional.ofNullable(user));
-        when(notificationDAO.getUserNotifications(notification.getUserId()))
-                .thenReturn(List.of(notification));
-
-        Optional<List<Notification>> result = notificationService.getUserNotifications(notification.getUserId());
-
-        assert(result.isPresent());
-        assertEquals(1, result.get().size());
-        verify(notificationDAO, times(1)).getUserNotifications(notification.getUserId());
-    }
-
-    @Test
-    void getUserNotifications_ReturnsListOfUserNotifications_WhenUserDoesNotExist() {
-        when(userService.findUserById(notification.getUserId()))
-                .thenReturn(Optional.empty());
-
-        Optional<List<Notification>> result = notificationService.getUserNotifications(notification.getUserId());
-
-        assert(result.isEmpty());
-        verify(notificationDAO, times(0)).getUserNotifications(notification.getUserId());
-    }
-
-    @Test
-    void getTaskNotifications_ReturnsListOfTaskNotifications_WhenTaskExists() {
-
-        when(taskService.getTaskById(notification.getTaskId()))
-                .thenReturn(Optional.of(task));
-        when(notificationDAO.getTaskNotifications(notification.getTaskId()))
-                .thenReturn(List.of(notification));
-
-        Optional<List<Notification>> result = notificationService.getTaskNotifications(notification.getTaskId());
-
-        assert(result.isPresent());
-        assertEquals(1, result.get().size());
-        verify(notificationDAO, times(1)).getTaskNotifications(notification.getTaskId());
-    }
-
-    @Test
-    void getTaskNotifications_ReturnsListOfTaskNotifications_WhenTaskDoesNotExist() {
-        when(taskService.getTaskById(notification.getTaskId()))
-                .thenReturn(Optional.empty());
-
-        Optional<List<Notification>> result = notificationService.getTaskNotifications(notification.getTaskId());
-
-        assert(result.isEmpty());
-        verify(notificationDAO, times(0)).getTaskNotifications(notification.getTaskId());
-    }
+//    @BeforeEach
+//    void setUp() {
+//        notification = new Notification(
+//                "test notification", 1L,
+//                LocalDateTime.now(), 1L);
+//        user = new User(
+//                1L,
+//                "Jane",
+//                "Doe",
+//                "test@gmail.com"
+//        );
+//        task = new Task(
+//                1L, "test task",
+//                LocalDateTime.now(), LocalDateTime.now(), true, 1L
+//        );
+//    }
+//
+//    @Test
+//    void getAllNotifications_ReturnsListOfNotifications_WhenNotEmpty() {
+//
+//        when(notificationDAO.getAllNotifications())
+//                .thenReturn(List.of(notification));
+//
+//        List<Notification> resNotifications = notificationService.getAllNotifications();
+//
+//        assertEquals(1, resNotifications.size());
+//        verify(notificationDAO, times(1)).getAllNotifications();
+//    }
+//
+//    @Test
+//    void getAllNotifications_ReturnsListOfNotifications_WhenEmpty() {
+//
+//        when(notificationDAO.getAllNotifications())
+//                .thenReturn(List.of());
+//
+//        List<Notification> resNotifications = notificationService.getAllNotifications();
+//
+//        assertEquals(0, resNotifications.size());
+//        verify(notificationDAO, times(1)).getAllNotifications();
+//    }
+//
+//    @Test
+//    void getUserNotifications_ReturnsListOfUserNotifications_WhenUserExists() {
+//        when(userService.findUserById(notification.getUserId()))
+//                .thenReturn(Optional.ofNullable(user));
+//        when(notificationDAO.getUserNotifications(notification.getUserId()))
+//                .thenReturn(List.of(notification));
+//
+//        Optional<List<Notification>> result = notificationService.getUserNotifications(notification.getUserId());
+//
+//        assert(result.isPresent());
+//        assertEquals(1, result.get().size());
+//        verify(notificationDAO, times(1)).getUserNotifications(notification.getUserId());
+//    }
+//
+//    @Test
+//    void getUserNotifications_ReturnsListOfUserNotifications_WhenUserDoesNotExist() {
+//        when(userService.findUserById(notification.getUserId()))
+//                .thenReturn(Optional.empty());
+//
+//        Optional<List<Notification>> result = notificationService.getUserNotifications(notification.getUserId());
+//
+//        assert(result.isEmpty());
+//        verify(notificationDAO, times(0)).getUserNotifications(notification.getUserId());
+//    }
+//
+//    @Test
+//    void getTaskNotifications_ReturnsListOfTaskNotifications_WhenTaskExists() {
+//
+//        when(taskService.getTaskById(notification.getTaskId()))
+//                .thenReturn(Optional.of(task));
+//        when(notificationDAO.getTaskNotifications(notification.getTaskId()))
+//                .thenReturn(List.of(notification));
+//
+//        Optional<List<Notification>> result = notificationService.getTaskNotifications(notification.getTaskId());
+//
+//        assert(result.isPresent());
+//        assertEquals(1, result.get().size());
+//        verify(notificationDAO, times(1)).getTaskNotifications(notification.getTaskId());
+//    }
+//
+//    @Test
+//    void getTaskNotifications_ReturnsListOfTaskNotifications_WhenTaskDoesNotExist() {
+//        when(taskService.getTaskById(notification.getTaskId()))
+//                .thenReturn(Optional.empty());
+//
+//        Optional<List<Notification>> result = notificationService.getTaskNotifications(notification.getTaskId());
+//
+//        assert(result.isEmpty());
+//        verify(notificationDAO, times(0)).getTaskNotifications(notification.getTaskId());
+//    }
 
 
 }
