49566fdc0888284fca040085440db4d2d3222fda
TamaraGG <goncharova2804t@gmail.com>
2025-05-08 12:03:48 +0300

    added registrationDate, added NonNull to email in User model

--- src/main/java/TGJavaProjects/TasksApplication/model/User.java
+++ src/main/java/TGJavaProjects/TasksApplication/model/User.java
@@ -2,17 +2,24 @@ package TGJavaProjects.TasksApplication.model;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
-    @NonNull
     private Long userId;
 
     private String firstName;
     private String lastName;
+
+    @NonNull
     private String email;
+
+    @NonNull
+    @Builder.Default
+    private LocalDateTime registrationDate = LocalDateTime.now();
 
 }
