97dc7bdc9ee4ccc7cb2406dc17ca4dc7b126bb52
TamaraGG <goncharova2804t@gmail.com>
2025-05-09 12:09:55 +0300

    added configuration for cache

--- src/main/java/TGJavaProjects/TasksApplication/config/CacheConfig.java
+++ src/main/java/TGJavaProjects/TasksApplication/config/CacheConfig.java
@@ -1,4 +1,44 @@
 package TGJavaProjects.TasksApplication.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+import java.time.Duration;
+
+@Configuration
 public class CacheConfig {
+
+    @Bean
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration(ObjectMapper redisObjectMapper) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer(redisObjectMapper)));
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer
+            (RedisCacheConfiguration defaultCacheConfiguration) {
+        return (builder) -> builder
+                .withCacheConfiguration("taskCache",
+                        defaultCacheConfiguration.entryTtl(Duration.ofMinutes(5)))
+                .withCacheConfiguration("userTasksCache",
+                        defaultCacheConfiguration.entryTtl(Duration.ofMinutes(2)));
+    }
 }
