1bc500c0a58c1bd3f47c2b62e9bfe17e38688359
TamaraGG <goncharova2804t@gmail.com>
2025-05-09 03:09:03 +0300

    added postgres

--- src/test/java/TGJavaProjects/TasksApplication/TasksApplicationTests.java
+++ src/test/java/TGJavaProjects/TasksApplication/TasksApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 //@ActiveProfiles("h2")
 @ActiveProfiles("in-memory")
+//@ActiveProfiles("pg")
 class TasksApplicationTests {
 
 	@Test
