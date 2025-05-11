e70c16199a30aa9bc3f16f08ca1f956998da7183
TamaraGG <goncharova2804t@gmail.com>
2025-05-11 03:30:49 +0300

    added annotations for scheduiling and async

--- src/test/java/TGJavaProjects/TasksApplication/TasksApplicationTests.java
+++ src/test/java/TGJavaProjects/TasksApplication/TasksApplicationTests.java
@@ -6,12 +6,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("h2")
 //@ActiveProfiles("in-memory")
+@EnableScheduling
+@EnableAsync
 class TasksApplicationTests {
 
 	@MockitoBean
